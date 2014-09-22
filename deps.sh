1b8ab2cae92b7aac34211909b27d4ebe595275d7
Huiba Li <lihuiba@gmail.com>
2014-09-22 14:43:10 +0800

    add python-pyblock

--- deps.sh
+++ deps.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-yum install -y protobuf-compiler protobuf-python redis python-redis libevent-devel iscsi-initiator-utils scsi-target-utils
+yum install -y protobuf-compiler protobuf-python redis python-redis libevent-devel iscsi-initiator-utils scsi-target-utils python-pyblock
 easy_install python-greenlet gevent
